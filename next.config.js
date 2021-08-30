4b4e3ef8117057cc9afcaaae0088b337de944e13
Fran Naranjo <fnaranjo88@gmail.com>
2021-08-30 13:05:34 +0100

    add response view in note and overlay

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 const nextTranslate = require('next-translate');
 
-module.exports = nextTranslate({});
+module.exports = nextTranslate({
+    eslint: {
+        dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+      },
+});
