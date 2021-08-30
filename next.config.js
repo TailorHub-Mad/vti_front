f47658d5bdf35555984772fd84c79bab5564c9e9
Fran Naranjo <fnaranjo88@gmail.com>
2021-08-30 13:18:08 +0100

    fix lint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
     eslint: {
-        dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+        dirs: ['src']
       },
 });
