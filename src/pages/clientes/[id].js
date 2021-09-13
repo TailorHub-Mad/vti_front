a6c1ec14b6bfe0bb5234d3ef89a0896109680d86
vctorChamizo <victorchamizo.r@gmail.com>
2021-09-13 16:22:49 +0200

    feat: refactor get object by id view

--- src/pages/clientes/[id].js
+++ src/pages/clientes/[id].js
@@ -1,53 +1,42 @@
 import { useRouter } from "next/dist/client/router"
-import { useEffect, useState } from "react"
+import { useContext } from "react"
 import { Page } from "../../components/layout/Pages/Page"
 import { PageHeader } from "../../components/layout/Pages/PageHeader/PageHeader"
 import { BreadCrumbs } from "../../components/navigation/BreadCrumbs/BreadCrumbs"
 import { Spinner } from "../../components/spinner/Spinner"
 import { ProjectsTable } from "../../views/projects/ProjectsTable/ProjectsTable"
 import { SWR_CACHE_KEYS } from "../../utils/constants/swr"
-import { useSWRConfig } from "swr"
 import useClientApi from "../../hooks/api/useClientApi"
 import { ToolBar } from "../../components/navigation/ToolBar/ToolBar"
+import { ApiAuthContext } from "../../provider/ApiAuthProvider"
+import useFetchSWR from "../../hooks/useFetchSWR"
 
 const client = () => {
   const router = useRouter()
-  const { cache } = useSWRConfig()
-  const { getClient: client } = useClientApi()
+  const { isLoggedIn } = useContext(ApiAuthContext)
+  const { getClient } = useClientApi()
 
-  const [clientData, setClientData] = useState(null)
-  const [clientNotFound, setClientNotFound] = useState(false)
+  const { data, error, isLoading, isValidating } = useFetchSWR(
+    [SWR_CACHE_KEYS.department, router.query.id],
+    getClient
+  )
 
-  const findClientInCache = (clients, id) => {
-    const client = clients.find((client) => client.id === id)
-    if (client) return setClientData(client)
-    getClient()
-  }
+  const notFound = !isValidating && !data
 
-  const getClient = async () => {
-    const data = await client(router.query.id)
-    data.length === 0 ? setClientNotFound(true) : setClientData(data[0])
-  }
-
-  useEffect(() => {
-    if (!cache.has(SWR_CACHE_KEYS.clients)) return getClient()
-    findClientInCache(cache.get(SWR_CACHE_KEYS.clients), router.query.id)
-  }, [cache])
-
-  // TOOD -> Manage error page
-  if (clientNotFound) return <>Error. No se ha encontrado el Client.</>
+  if (error) return <>ERROR...</>
+  if (!isLoggedIn) return <>Loading...</>
   return (
     <Page>
-      {clientData ? (
+      {isLoading || !data ? <Spinner /> : null}
+      {notFound && <>Error. No se ha encontrado el cliente.</>}
+      {data && (
         <>
           <PageHeader>
             <BreadCrumbs lastText="Proyectos" />
             <ToolBar />
           </PageHeader>
-          <ProjectsTable items={clientData.projects} />
+          <ProjectsTable items={data.projects} />
         </>
-      ) : (
-        <Spinner />
       )}
     </Page>
   )
--- src/pages/sistemas/[id].js
+++ src/pages/sistemas/[id].js
@@ -1,58 +1,42 @@
 import { useRouter } from "next/dist/client/router"
-import { useEffect, useState } from "react"
+import { useContext } from "react"
 import { Page } from "../../components/layout/Pages/Page"
 import { PageHeader } from "../../components/layout/Pages/PageHeader/PageHeader"
 import { BreadCrumbs } from "../../components/navigation/BreadCrumbs/BreadCrumbs"
 import { Spinner } from "../../components/spinner/Spinner"
 import { ProjectsTable } from "../../views/projects/ProjectsTable/ProjectsTable"
 import { SWR_CACHE_KEYS } from "../../utils/constants/swr"
-import { useSWRConfig } from "swr"
 import useSystemApi from "../../hooks/api/useSystemsApi"
 import { ToolBar } from "../../components/navigation/ToolBar/ToolBar"
+import useFetchSWR from "../../hooks/useFetchSWR"
+import { ApiAuthContext } from "../../provider/ApiAuthProvider"
 
 const system = () => {
   const router = useRouter()
-  const { cache } = useSWRConfig()
-  const { system } = useSystemApi()
+  const { isLoggedIn } = useContext(ApiAuthContext)
+  const { getDepartment } = useSystemApi()
 
-  const [systemData, setSystemData] = useState(null)
-  const [systemNotFound, setSystemNotFound] = useState(false)
+  const { data, error, isLoading, isValidating } = useFetchSWR(
+    [SWR_CACHE_KEYS.department, router.query.id],
+    getDepartment
+  )
 
-  const findSystemInCache = (systems, id) => {
-    const system = systems.find((system) => system.id === id)
-    if (system) return setSystemData(system)
-    getSystem()
-  }
+  const notFound = !isValidating && !data
 
-  const getSystem = async () => {
-    const data = await system(router.query.id)
-    data.length === 0
-      ? setSystemNotFound(true)
-      : setSystemData(data[0].testSystems[0])
-  }
-
-  useEffect(() => {
-    if (!cache.has(SWR_CACHE_KEYS.systems)) return getSystem()
-    findSystemInCache(
-      cache.get(SWR_CACHE_KEYS.systems)[0].testSystems,
-      router.query.id
-    )
-  }, [cache])
-
-  // TOOD -> Manage error page
-  if (systemNotFound) return <>Error. No se ha encontrado el Sistema.</>
+  if (error) return <>ERROR...</>
+  if (!isLoggedIn) return <>Loading...</>
   return (
     <Page>
-      {systemData ? (
+      {isLoading || !data ? <Spinner /> : null}
+      {notFound && <>Error. No se ha encontrado el sistema.</>}
+      {data && (
         <>
           <PageHeader>
             <BreadCrumbs lastText="Proyectos" />
             <ToolBar />
           </PageHeader>
-          <ProjectsTable items={systemData.projects} />
+          <ProjectsTable items={data.projects} />
         </>
-      ) : (
-        <Spinner />
       )}
     </Page>
   )
