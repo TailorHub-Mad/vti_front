e92dca779a9b226208d53a033f6c72568c567f90
Fran Naranjo <fnaranjo88@gmail.com>
2021-10-07 17:02:06 +0200

    fix label styles

--- src/theme/components/formLabel.theme.js
+++ src/theme/components/formLabel.theme.js
@@ -1,21 +1,19 @@
 export const FormLabel = {
   variants: {
     base: {
-        fontFamily: "Noway-Medium",
-        fontSize: ["14px", "14px"],
-        lineHeight: ["16px", "16px"],
-        marginBottom: "2.5px !important",
-        _disabled: {
-          _hover: {
-            borderColor: "light_grey"
-          },
-          opacity: 0.6
+      fontFamily: "Noway-Medium",
+      fontSize: ["14px", "14px"],
+      lineHeight: ["16px", "16px"],
+      marginBottom: "2.5px !important",
+      _disabled: {
+        _hover: {
+          borderColor: "light_grey"
         },
-    },
-    
+        opacity: 0.6
+      }
+    }
   },
   defaultProps: {
     variant: "base"
   }
 }
-
