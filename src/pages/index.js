bf5ee4e9bd2806e4b1eb0a3805dc169a28d39137
vctorChamizo <victorchamizo.r@gmail.com>
2021-10-07 14:02:14 +0200

    feat: manage edti message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 import { ApiAuthContext } from "../provider/ApiAuthProvider"
-import { PATHS } from "../utils/constants/global"
+import { PATHS, RoleType } from "../utils/constants/global"
 import { LoadingView } from "../views/common/LoadingView"
 
 const Index = () => {
   const router = useRouter()
-  const { isLoggedIn } = useContext(ApiAuthContext)
+  const { isLoggedIn, role } = useContext(ApiAuthContext)
 
   useEffect(() => {
     if (!isLoggedIn) return
-    router.push(PATHS.notes)
+    role === RoleType.USER ? router.push(PATHS.newNote) : router.push(PATHS.notes)
   }, [isLoggedIn])
 
   return <LoadingView />
