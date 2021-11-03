db96045e8a5b7b43b594319cb8023d1cc1a743ac
vctorChamizo <victorchamizo.r@gmail.com>
2021-11-03 13:43:00 +0100

    fix: mobile

--- src/components/overlay/Modal/CustomModalHeader/CustomModalHeader.js
+++ src/components/overlay/Modal/CustomModalHeader/CustomModalHeader.js
@@ -4,7 +4,14 @@ import { CloseIcon } from "../../../icons/CloseIcon"
 
 export const CustomModalHeader = ({ onClose, title, ...props }) => {
   return (
-    <Flex justify="space-between" align="center" height="33px" {...props}>
+    <Flex
+      justify="space-between"
+      align="center"
+      height="33px"
+      {...props}
+      mt={["32px", null, null, "0"]}
+      mb={["24px", null, null, "0"]}
+    >
       <Text variant="d_l_medium">{title}</Text>
       {onClose && <CloseIcon cursor="pointer" onClick={onClose} />}
     </Flex>
