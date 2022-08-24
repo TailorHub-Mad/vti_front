398e86d9a140c37f115052420ddfd816f2f5ef4b
Gabriel <kunry3@hotmail.com>
2022-08-24 17:00:23 +0200

    fix: add ref in apuntes

--- src/components/cards/MessageCard/MessageCardInfo/MessageCardInfo.js
+++ src/components/cards/MessageCard/MessageCardInfo/MessageCardInfo.js
@@ -1,20 +1,19 @@
 import { Flex, Text } from "@chakra-ui/react"
 import React from "react"
 
-export const MessageCardInfo = ({ id, author, updatedAt, isAdmin, ...props }) => {
+export const MessageCardInfo = ({ id, author, updatedAt, ...props }) => {
   return (
     <Flex {...props} width="100%" justifyContent="space-between">
-      {isAdmin && id ? (
         <Text color="grey" variant="d_xs_regular">
           {id}
         </Text>
-      ) : null}
 
       <Text
         color="grey"
         isTruncated
         variant="d_xs_regular"
-        marginLeft={isAdmin && id ? "10px" : "0"}
+        // marginLeft={isAdmin && id ? "10px" : "0"}
+        marginLeft={"0"}
       >
         {author}
       </Text>
